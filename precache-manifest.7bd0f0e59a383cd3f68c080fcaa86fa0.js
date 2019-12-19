91d1e09bdad3a128bd51066b5c5da797c4d13224
David Aguilar <davidaguilarh.94@gmail.com>
2019-12-19 02:25:12 -0500

    Updates

---
+++ precache-manifest.7bd0f0e59a383cd3f68c080fcaa86fa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b24145db59a3ee31f7afd69fe39eed63",
+    "url": "/currencyXchange/index.html"
+  },
+  {
+    "revision": "9a6fbc878638f1ac4a42",
+    "url": "/currencyXchange/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "af44b1dc7930c7da618e",
+    "url": "/currencyXchange/static/css/main.c5d0dc2d.chunk.css"
+  },
+  {
+    "revision": "9a6fbc878638f1ac4a42",
+    "url": "/currencyXchange/static/js/2.9de45849.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/currencyXchange/static/js/2.9de45849.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af44b1dc7930c7da618e",
+    "url": "/currencyXchange/static/js/main.2847da10.chunk.js"
+  },
+  {
+    "revision": "55aa354627fd5721c04e",
+    "url": "/currencyXchange/static/js/runtime-main.bbe26a56.js"
+  }
+]);
